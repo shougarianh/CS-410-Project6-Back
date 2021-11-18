ed9afc6e31e65079d2d2e19e62fa4351a841254d
shougarianh <shougarianh@gmail.com>
2021-11-18 13:31:27 -0500

    working

--- calculations.go
+++ calculations.go
@@ -1,1 +1,24 @@
+package main
+
+import "fmt"
+
 // Contains functions that do property calculations
+
+func totalIncome(gorss_rents, parking, storage, laundry_vending, other float64) float64 {
+	return gorss_rents + parking + storage + laundry_vending + other
+}
+
+func vacancyLoss(vacancy_rate, total_income float64) float64 {
+	return vacancy_rate * total_income
+}
+
+/*
+func effectiveGrossIncome(total_income, vacancy_loss, float64) float64
+{
+	return total_income - vacancy_loss
+}
+*/
+
+func main() {
+	fmt.Println(vacancyLoss(0.05, 14400))
+}
