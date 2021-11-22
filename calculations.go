6307404ab1006097acb916aec8169ec31b9db857
shougarianh <shougarianh@gmail.com>
2021-11-21 19:18:28 -0500

    finished cashFlowPerUnitPerMonth

--- calculations.go
+++ calculations.go
@@ -29,7 +29,7 @@ func firstMtgTotalPrinciple(first_mtg_principle_borrowed, first_mtg_CMHC_fee flo
 }
 
 func firstMtgTotalMonthlyPayment(first_mtg_total_principle, first_mtg_interest_rate, first_mtg_amortization_period float64) float64 {
-	var percentage float64 = first_mtg_interest_rate / 100
+	var percentage float64 = first_mtg_interest_rate / 100.0
 	return first_mtg_total_principle * (math.Pow((1+(percentage/2)), (1.0/6.0)) - 1) / (1 - math.Pow((math.Pow((1+(percentage/2)), (1.0/6.0))), (first_mtg_amortization_period*(-12))))
 }
 
@@ -54,8 +54,13 @@ func totalMonthlyProfitLoss(anual_profit_or_loss float64) float64 {
 	return anual_profit_or_loss / 12
 }
 
+func cashFlowPerUnitPerMonth(total_monthly_profit_loss, number_of_units float64) float64 {
+	return total_monthly_profit_loss / number_of_units
+}
+
 func main() {
 	fmt.Println(firstMtgTotalMonthlyPayment(78750, 3.63, 30))
 	fmt.Println(debtServiceCosts(357.94, 0, 0, 0))
+	fmt.Println(totalMonthlyProfitLoss(4604))
 
 }
